59ddb51bf276044f290ca4c484d3f632bc92f0ab
Leo <leoreyes1001@gmail.com>
2014-01-21 20:05:03 -0600

    Server class

---
+++ src/Server.java
@@ -0,0 +1,17 @@
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	private ServerSocket serverSock;
+	private Socket deviceSock;
+	
+	public Server() {
+		
+	}
+	
+	public static void main(String args[]) {
+
+	}
+}

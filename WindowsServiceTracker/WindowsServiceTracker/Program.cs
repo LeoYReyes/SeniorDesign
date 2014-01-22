5f52248591f9df6b89da1af4e1b79fae5ba77830
Nathan <nwp0002@auburn.edu>
2014-01-22 10:19:09 -0600

    Changed main to include install and uninstall options

--- WindowsServiceTracker/WindowsServiceTracker/Program.cs
+++ WindowsServiceTracker/WindowsServiceTracker/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration.Install; //used for ManagedInstallerClass
+using System.Reflection; //used for Assembly class
 
 namespace WindowsServiceTracker
 {
@@ -12,14 +14,38 @@ namespace WindowsServiceTracker
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static void Main()
+        static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-            { 
-                new Tracker() 
-            };
-            ServiceBase.Run(ServicesToRun);
+            /**********************************************
+             * Since we have to install the service every time we test it I found
+             * this code online to help with that. Run the WindowsServiceTracker.exe
+             * from command line and either use the argument "--install" to install
+             * the service or use "--uninstall" to uninstall the service. Don't use
+             * any arguments if you just want to run the service normally once it's
+             * been installed.
+             *********************************************/
+            if (Environment.UserInteractive)
+            {
+                string parameter = string.Concat(args);
+                switch (parameter)
+                {
+                    case "--install":
+                        ManagedInstallerClass.InstallHelper(new[] { Assembly.GetExecutingAssembly().Location });
+                        break;
+                    case "--uninstall":
+                        ManagedInstallerClass.InstallHelper(new[] { "/u", Assembly.GetExecutingAssembly().Location });
+                        break;
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+                { 
+                    new Tracker() 
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
         }
     }
 }

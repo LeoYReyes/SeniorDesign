19e9004b19f1b275154275450295043ff5f02894
Leo <leoreyes1001@gmail.com>
2014-01-21 19:54:03 -0600

    whatttttt the heckkkkk

--- src/Server.java
+++ src/Server.java
@@ -3,18 +3,6 @@
  * version: 1/20/2014
  */
 
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Server {
-	
-	private ServerSocket serverSock;
-	private Socket deviceSock;
-	
-	public Server() {
-		
-	}
-	
-	
-	
+
 }

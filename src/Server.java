abbf531a98508e96908e6fb0c3924a46251017e1
Leo <leoreyes1001@gmail.com>
2014-01-21 20:07:48 -0600

    morreee

--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,32 @@
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
-	private ServerSocket serverSock;
-	private Socket deviceSock;
+	private static ServerSocket serverSock;
+	private static Socket deviceSock;
 	
 	public Server() {
 		
 	}
 	
 	public static void main(String args[]) {
-
+		int portNumber = 10000;
+		try {
+			serverSock = new ServerSocket(portNumber);
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+		
+		while(true) {
+			try {
+				deviceSock = serverSock.accept();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
 	}
 }

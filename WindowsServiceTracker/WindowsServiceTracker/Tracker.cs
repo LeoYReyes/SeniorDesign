5423e151dc04e12ef84f8a28bd4af1eb4f29a426
Nathan <nwp0002@auburn.edu>
2014-01-26 16:11:53 -0600

    Added correct code and form for writing to the event logs

--- WindowsServiceTracker/WindowsServiceTracker/Tracker.cs
+++ WindowsServiceTracker/WindowsServiceTracker/Tracker.cs
@@ -24,6 +24,9 @@ namespace WindowsServiceTracker
         //127.0.0.1 = 0x0100007F because of network byte order
         private const long IP_ADDRESS = 0x0100007F; //127.0.0.1 as placeholder
         private const int PORT = 10000;
+        private const string errorLogName = "TrackerErrorLog";
+        private const string errorLogMachine = "TrackerComputer";
+        private const string errorLogSource = "WindowsServiceTracker";
 
         private IPEndPoint IPPort = new IPEndPoint(IP_ADDRESS, PORT);
         private TcpClient tcp;
@@ -31,6 +34,11 @@ namespace WindowsServiceTracker
         public Tracker()
         {
             InitializeComponent();
+            //Creates the error log source if it doesn't already exist
+            if(!EventLog.SourceExists(errorLogSource))
+            {
+                EventLog.CreateEventSource(errorLogSource, errorLogName);
+            }
         }
 
         protected override void OnStart(string[] args)
@@ -43,9 +51,21 @@ namespace WindowsServiceTracker
             //Keep the service running for 15 seconds
             Thread.Sleep(15000);
 
+            //Write to the Windows Event Logs, shows up under Windows Logs --> Application
+            EventLog.WriteEntry(errorLogSource, "Test event", EventLogEntryType.Information);
+
             //Some test tcp connection stuff
-            tcp = new TcpClient();
-            tcp.Connect(IPPort);
+            try
+            {
+                tcp = new TcpClient();
+                tcp.Connect(IPPort);
+            }
+            finally
+            {
+                //Another write to the Windows Event Logs, shows up in the same place as before but as type "Error" instead of type "Information"
+                string error = "Service failed to connect to IP address " + IP_ADDRESS + " on port " + PORT;
+                EventLog.WriteEntry(errorLogSource, error, EventLogEntryType.Error);
+            }
             NetworkStream tcpStream = tcp.GetStream();
             if (tcpStream.CanWrite)
             {

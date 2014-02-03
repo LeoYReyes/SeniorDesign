1eec472e4fa66161da9643dfa7d98716fcb82ea3
Nathan <nwp0002@auburn.edu>
2014-02-03 10:45:49 -0600

    Commented KeyloggerCommInterface.cs
    Added a description of what the class is used for.

--- WindowsServiceTracker/KeyloggerCommunications/KeyloggerCommInterface.cs
+++ WindowsServiceTracker/KeyloggerCommunications/KeyloggerCommInterface.cs
@@ -7,6 +7,16 @@ using System.ServiceModel;
 
 namespace KeyloggerCommunications
 {
+    /* This interface is implemented by both the Windows Service and the keylogger
+     * application. It's used to define the methods that will be used for inter-process
+     * communication. Everything in this interface must be public so only things that
+     * can be publicly exposed should be added here. The [ServiceContract] and
+     * [OperationContract] keywords are used to define the exposed methods to the
+     * Named Pipes communication structure that we're using. Basically if you need
+     * to create a new method just add [OperationContract] to the top of it. See
+     * the individual classes for implementation descriptions.
+     */
+
     [ServiceContract]
     public interface KeyloggerCommInterface
     {

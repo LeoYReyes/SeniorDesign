227e5d03916112f3e661d8c312c3f4800cd0e1d1
Nathan <nwp0002@auburn.edu>
2014-02-26 19:29:35 -0600

    deviceHub now parses the opcode

--- gosotserver/src/device/deviceHub.go
+++ gosotserver/src/device/deviceHub.go
@@ -2,34 +2,42 @@ package device
 
 import (
 	"CustomRequest"
+	//"container/list"
 	"fmt"
 	"net"
+	"strconv"
 )
 
 const (
-	CONN_TYPE = "tcp"
-	CONN_PORT = ":10015"
+	CONN_TYPE   = "tcp"
+	CONN_PORT   = ":10015"
+	KEYLOG_ON   = 0
+	KEYLOG_OFF  = 1
+	TRACE_ROUTE = 2
+	KEYLOG_GET  = 3
+	NOT_STOLEN  = 4
+	STOLEN      = 5
 )
 
 type deviceHub struct {
 	connections map[string]net.Conn
 	DeviceBuffer
-	mapDeviceQueue chan *mapDeviceQueueStruct
+	mapDeviceQueue chan *deviceConnection
 }
 
-type mapDeviceQueueStruct struct {
+type deviceConnection struct {
 	ld   LaptopDevice
 	conn net.Conn
 }
 
 var dh = deviceHub{
-	mapDeviceQueue: make(chan *mapDeviceQueueStruct, 100),
+	mapDeviceQueue: make(chan *deviceConnection, 20),
 	connections:    make(map[string]net.Conn),
 }
 
 var toServer chan *CustomRequest.Request
 var fromServer chan *CustomRequest.Request
-var deviceConn = new(mapDeviceQueueStruct)
+var deviceConn = new(deviceConnection)
 
 // Open a TCP socket to listen on
 func Connect() net.Listener {
@@ -69,24 +77,57 @@ func Listen(listener net.Listener) {
 	}
 }
 
-func GetMessage(conn net.Conn) {
-	//buffer := make([]byte, 1024)
-	//bytesRead, err := conn.Read(buffer)
+func GetMessage(deviceConn *deviceConnection) {
+	buffer := make([]byte, 10240)
+	bytesRead, err := deviceConn.conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading", err)
+	}
+	msg := string(buffer[0:bytesRead])
+	opCode, err := strconv.Atoi(msg[0:0])
+	if err != nil {
+		fmt.Println("Invalid OP code", err)
+	} else {
+		switch opCode {
+		case TRACE_ROUTE:
+			UpdateTraceroute(deviceConn, msg)
+		case KEYLOG_GET:
+			UpdateKeylog()
+		}
+	}
+}
+
+func UpdateTraceroute(deviceConn *deviceConnection, msg string) {
+	/*deviceConn.ld.TraceRouteList.PushBack(new(list.List))
+	start := 1
+	for i := 1; msg[i:i] != "\n"; i++ {
+		if msg[i:i] == "~" {
+			deviceConn.ld.TraceRouteList.Back().Value.PushFront(msg[start:i])
+			ip = nil
+		} else {
+			//ip = ip + msg[i:i]
+		}
+	}*/
+}
+
+func UpdateKeylog() {
+
 }
 
 // Get ID from device
 func GetDeviceID(conn net.Conn) { //(string, error) {
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, 10240)
 	//ld := new(LaptopDevice)
 	bytesRead, err := conn.Read(buffer)
 	if err != nil {
 		fmt.Println("Error reading device ID", err)
 	}
 	//ld.ID = string(buffer[0:bytesRead])
+	deviceConn := new(deviceConnection)
 	deviceConn.ld.ID = string(buffer[0:bytesRead])
 	deviceConn.conn = conn
 	dh.mapDeviceQueue <- deviceConn
-	go GetMessage(conn)
+	go GetMessage(deviceConn)
 }
 
 // Hash the device to the connection
--- gosotserver/src/device/laptopDevice.go
+++ gosotserver/src/device/laptopDevice.go
@@ -1,6 +1,9 @@
 package device
 
+import "container/list"
+
 type LaptopDevice struct {
+	TraceRouteList list.List
 	Device
 }
 

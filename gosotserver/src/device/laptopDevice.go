cb4d65a794b005981c37ccf12a1f6a21c00df732
Leo <leoreyes1001@gmail.com>
2014-03-01 17:19:26 -0600

    Revert "Added database requests in laptopDevice.go"
    This reverts commit 9c6051564318246b8195e2fb9924c69ff13d3433.

--- gosotserver/src/device/laptopDevice.go
+++ gosotserver/src/device/laptopDevice.go
@@ -6,11 +6,8 @@
 
 package device
 
-import (
-	"CustomProtocol"
-	"container/list"
-	"fmt"
-)
+import "container/list"
+import "CustomProtocol"
 
 type LaptopDevice struct {
 	TraceRouteList list.List
@@ -30,49 +27,10 @@ func (ld *LaptopDevice) CheckIfStolen() bool {
 		Source: CustomProtocol.DeviceLaptop, OpCode: CustomProtocol.CheckDeviceStolen, Payload: id,
 		Response: response}
 	toServer <- req
-	fmt.Println("CheckIfStolen request created and sent")
 	isStolen := <-response
 	if isStolen[0] == 1 {
 		return true
 	} else {
 		return false
 	}
-}
-
-func (ld *LaptopDevice) UpdateKeylog() bool {
-	id := []byte(ld.Device.ID)
-	keylog := ld.KeylogData.Back().Value.(string)
-	payload := append(id, keylog...)
-	response := make(chan []byte)
-	req := &CustomProtocol.Request{Id: CustomProtocol.AssignRequestId(), Destination: CustomProtocol.Database,
-		Source: CustomProtocol.DeviceLaptop, OpCode: CustomProtocol.CheckDeviceStolen, Payload: payload,
-		Response: response}
-	toServer <- req
-	fmt.Println("UpdateKeylogData request created and sent")
-	KeylogUpdated := <-response
-	if KeylogUpdated[0] == 1 {
-		return true
-	} else {
-		return false
-	}
-}
-
-func (ld *LaptopDevice) UpdateTraceroute() bool {
-	id := []byte(ld.Device.ID)
-	traceroute := ld.TraceRouteList.Back().Value.(string)
-	tracerouteBytes := []byte(traceroute)
-	payload := append(id, tracerouteBytes...)
-	response := make(chan []byte)
-	req := &CustomProtocol.Request{Id: CustomProtocol.AssignRequestId(), Destination: CustomProtocol.Database,
-		Source: CustomProtocol.DeviceLaptop, OpCode: CustomProtocol.CheckDeviceStolen, Payload: payload,
-		Response: response}
-	toServer <- req
-	fmt.Println("UpdateTracerouteData request created and sent")
-	TracerouteUpdated := <-response
-	if TracerouteUpdated[0] == 1 {
-		return true
-	} else {
-		return false
-	}
-	//TODO possibly only deserialize the IP list when displaying on the webpage
 }

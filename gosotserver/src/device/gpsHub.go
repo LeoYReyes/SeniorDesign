196f11fe8746e4a651229e983ce4e419a33048b1
Leo <leoreyes1001@gmail.com>
2014-03-01 17:19:55 -0600

    Revert "Split smsconnection into multiple functions"
    This reverts commit 5a9b690a857242e987488602b23d4134a4f533bd.

--- gosotserver/src/device/gpsHub.go
+++ gosotserver/src/device/gpsHub.go
@@ -18,55 +18,54 @@ import (
 	"strings"
 )
 
-var toServerT chan []byte
+var smsConn net.Conn
+var smsCh = make(chan []byte)
 
-func GPSConnect() net.Listener {
+func SmsConnection() {
+	//connect
 	listener, err := net.Listen(CONN_TYPE, CONN_PORT_SMS)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 	} else {
 
 	}
-	fmt.Println("Connection created on " + CONN_TYPE + " " + CONN_PORT_SMS)
-	return listener
-}
 
-func GPSListen(listener net.Listener) net.Conn {
+	fmt.Println("Connection created on " + CONN_TYPE + " " + CONN_PORT_SMS)
+
+	//send & receive
 	for {
-		conn, err := listener.Accept()
+		smsConn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error connecting", err)
 		}
-		fmt.Println("Connection established with SMS client")
-		GPSGetMessages(conn)
-	}
-}
-
-func GPSGetMessages(conn net.Conn) {
-	msg := ""
-	for {
 		buffer := make([]byte, 512)
-		bytesRead, _ := conn.Read(buffer)
-		if bytesRead > 0 {
-			if bytesRead > 10 {
-				received := string(buffer[0:bytesRead])
-				msg = googleMapLinkParser(received)
-				fmt.Println("Received msg: ", msg)
-				req := &CustomProtocol.Request{Id: CustomProtocol.AssignRequestId(), Destination: CustomProtocol.Web, Source: CustomProtocol.DeviceGPS,
-					OpCode: CustomProtocol.UpdateWebMap, Payload: []byte(msg)}
-				toServer <- req
-			} else {
-				conn.Write([]byte("|"))
+		fmt.Println("Connection established with SMS client")
+
+		msg := ""
+		for {
+			select {
+			case m := <-smsCh:
+				fmt.Println("smsCh: ", string(m))
+				smsConn.Write(m)
+			default:
+				//fmt.Println("Waiting to read from smsdevice")
+				bytesRead, _ := smsConn.Read(buffer)
+				if bytesRead > 0 {
+					if bytesRead > 10 {
+						received := string(buffer[0:bytesRead])
+						msg = googleMapLinkParser(received)
+						fmt.Println("Received msg: ", msg)
+						req := &CustomProtocol.Request{Id: CustomProtocol.AssignRequestId(), Destination: CustomProtocol.Web, Source: CustomProtocol.DeviceGPS,
+							OpCode: CustomProtocol.UpdateWebMap, Payload: []byte(msg)}
+						toServer <- req
+						fmt.Println("Req sent to server")
+					} else {
+						smsConn.Write([]byte("|"))
+					}
+				}
 			}
 		}
 	}
-}
-
-func SmsConnection() {
-	//connect
-	listener := GPSConnect()
-	GPSListen(listener)
-	//send & receive
 
 }
 

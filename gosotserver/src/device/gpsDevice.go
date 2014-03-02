1f87617bec0696481f787c3a22bec6ca3ac940fe
Nathan <nwp0002@auburn.edu>
2014-03-02 00:00:16 -0600

    More fucking comments jesus christ

--- gosotserver/src/device/gpsDevice.go
+++ gosotserver/src/device/gpsDevice.go
@@ -1,9 +1,15 @@
+/*
+ * @author Nathan Plotts (nwp0002@auburn.edu)
+ * This file is where the structure for a GPSDevice is defined. Any functions
+ * specific to the GPSDevice will also be defined here.
+ */
+
 package device
 
 type GPSDevice struct {
 	Device
 }
 
-type GPSClient struct {
+/*type GPSClient struct {
 	Client
-}
+}*/
--- gosotserver/src/device/laptopDevice.go
+++ gosotserver/src/device/laptopDevice.go
@@ -1,7 +1,7 @@
 /*
  * @author Nathan Plotts (nwp0002@auburn.edu)
  * This file is where the the LaptopDevice struct is defined and
- * also where LaptopDevice specific functions will be stored.
+ * also where LaptopDevice specific functions are stored.
  */
 
 package device
@@ -12,16 +12,28 @@ import (
 	"fmt"
 )
 
+/*
+ * This struct defines the structure of a connected laptop device. It contains a list
+ * of strings that contain IP traceroutes and a list of strings that contain keylog
+ * data.
+ */
 type LaptopDevice struct {
 	TraceRouteList list.List
 	KeylogData     list.List
 	Device
 }
 
-type LaptopClient struct {
+/*type LaptopClient struct {
 	Client
-}
+}*/
 
+/*
+ * This function creates and sends a request to the database to check if the connected
+ * laptop is stolen. If it is stolen it sends a message back to the laptop alerting it
+ * that it has been marked as stolen and needs to start tracking. If the request says
+ * that the laptop is not stolen it sends a message to the laptop letting it know that
+ * it is not stolen.
+ */
 func (ld *LaptopDevice) CheckIfStolen() bool {
 	//TODO send database request here
 	id := []byte(ld.Device.ID)
@@ -39,6 +51,11 @@ func (ld *LaptopDevice) CheckIfStolen() bool {
 	}
 }
 
+/*
+ * This function creates and sends a request to the database to write new keylog data
+ * to the device's logs. The database function would then respond with boolean letting
+ * the function know if the write succeeded.
+ */
 func (ld *LaptopDevice) UpdateKeylog() bool {
 	id := []byte(ld.Device.ID)
 	keylog := ld.KeylogData.Back().Value.(string)
@@ -58,6 +75,11 @@ func (ld *LaptopDevice) UpdateKeylog() bool {
 	}
 }
 
+/*
+ * This function creates and sends a request to the database to write a new IP traceroute
+ * string to the device's logs. The database then response with a boolean to let the
+ * function know if the write succeeded.
+ */
 func (ld *LaptopDevice) UpdateTraceroute() bool {
 	id := []byte(ld.Device.ID)
 	traceroute := ld.TraceRouteList.Back().Value.(string)

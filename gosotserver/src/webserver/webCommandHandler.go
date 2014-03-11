dcddb43d4d471b33a83c3a055ba789c46262523d
Leo <leoreyes1001@gmail.com>
2014-03-11 11:36:20 -0500

    webCommands update

--- gosotserver/src/webserver/webCommandHandler.go
+++ gosotserver/src/webserver/webCommandHandler.go
@@ -13,6 +13,21 @@ func newDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(r.PostForm)
+	buf := []byte{}
+	// Device Id (phone number for Geogram, MAC Address for laptops)
+	buf = append(buf, []byte(r.PostForm.Get("deviceId"))...)
+	buf = append(buf, 0x1B)
+	// Device type
+	buf = append(buf, []byte(r.PostForm.Get("deviceType"))...)
+	buf = append(buf, 0x1B)
+	// Device name
+	buf = append(buf, []byte(r.PostForm.Get("deviceName"))...)
+	buf = append(buf, 0x1B)
+	// Device owner, get user account info from session
+	session, _ := store.Get(r, "userSession")
+	buf = append(buf, []byte(session.Values["userId"])...) // NEEDS FIX
+	buf = append(buf, 0x1B)
+
 	if err != nil {
 		// Handle error
 		fmt.Println("decoder error: ", err)

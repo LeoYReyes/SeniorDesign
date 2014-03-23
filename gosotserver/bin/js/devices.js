1c188ef140cfd2ed82b2f62a9d60fe706df894d3
Nathan <nwp0002@auburn.edu>
2014-03-23 16:48:47 -0500

    Updated bin webpage assets

---
+++ gosotserver/bin/js/devices.js
@@ -0,0 +1,65 @@
+$(function() {
+	var deviceType = $( "#deviceType" ),
+		deviceName = $( "#deviceName" ),
+      	deviceId = $( "#deviceId" );
+		allFields = $( [] ).add( deviceType ).add( deviceId ).add( deviceName );
+	function checkRegexp( o, regexp, n ) {
+		  if ( !( regexp.test( o.val() ) ) ) {
+			o.addClass( "ui-state-error" );
+			updateTips( n );
+			return false;
+		  } else {
+			return true;
+		  }
+		}
+ 
+		$( "#dialog-form" ).dialog({
+		  autoOpen: false,
+		  height: 430,
+		  width: 450,
+		  modal: true,
+		  buttons: {
+			"Add New Device": function() {
+			  var bValid = true;
+			  allFields.removeClass( "ui-state-error" );
+			  $.ajax({
+				  url:"/newDevice",
+				  type: "POST",
+				  data: { 
+					deviceType: deviceType.val(),
+					deviceId: deviceId.val(),
+					deviceName: deviceName.val(),
+				  },
+			  }).done(function(response) {
+				alert(response);
+				$("#dialog-form").dialog("close");
+			  });
+			},
+			Cancel: function() {
+			  $( this ).dialog( "close" );
+			}
+		  },
+		  close: function() {
+			deviceName.val("");
+			deviceId.val("");
+			deviceType.prop('checked', false);
+			allFields.removeClass( "ui-state-error" );
+		  }
+		});
+ 
+		$( "#newDeviceButton" )
+		  .click(function() {
+			$( "#dialog-form" ).dialog( "open" );
+		});
+		$("input:radio[name=deviceType]").click(function() {
+    		deviceType = $(this);
+		});
+		$( "#deviceOne" )
+		  .click(function() {
+			$("#deviceOneBox").toggle();
+		});
+		$( "#deviceTwo" )
+		  .click(function() {
+			$(this).toggle();
+		});
+});

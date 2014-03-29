dff44a1f7c18453afa48494fa2935e7810027569
denneyburk <vitamindenknee@aol.com>
2014-03-28 21:58:15 -0500

    creating and downloading the key logger text file

--- gosotserver/src/server/js/devices.js
+++ gosotserver/src/server/js/devices.js
@@ -147,4 +147,38 @@ $(function() {
 			deviceDiv.append(row);
 			return deviceDiv;
 		}
+		
+		
+
+			
+		
+		
 });
+
+
+function showKeylog(){
+	
+	Downloadify.create('downloadify',{
+		
+		filename: function(){
+			return document.getElementById('filename').value;
+		},
+		
+		data: function(){ 
+			return document.getElementById('data').value;
+		},
+		
+		onComplete: function(){ alert('Your File Has Been Saved!'); },
+		onCancel: function(){ alert('You have cancelled the saving of this file.'); },
+		onError: function(){ alert('You must put something in the File Contents or there will be nothing to save!'); },
+		
+		swf: 'media/downloadify.swf',
+		downloadImage: 'images/download.png',
+		width: 100,
+		height: 30,
+		transparent: true,
+		append: false
+		
+	});
+	
+}

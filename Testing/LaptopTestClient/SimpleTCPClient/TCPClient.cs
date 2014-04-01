c61de668538eb6a601ba19dd917e474528d5677b
Nathan <nwp0002@auburn.edu>
2014-03-31 22:46:36 -0500

    Updated laptop/server tests

--- Testing/LaptopTestClient/SimpleTCPClient/TCPClient.cs
+++ Testing/LaptopTestClient/SimpleTCPClient/TCPClient.cs
@@ -40,7 +40,7 @@ namespace SimpleTCPClient
             Console.WriteLine("Received stolen code: {0}", stolenCode);
         }
 
-        /*static void test2()
+        static void test2()
         {
             Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             sender.Connect(remoteEP);
@@ -55,16 +55,28 @@ namespace SimpleTCPClient
             int stolenCode = buffer[0];
             Console.WriteLine("Received stolen code: {0}", stolenCode);
 
-            msg = Encoding.ASCII.GetBytes("this is new keylog data\n");
-            byte[] msgWithOpcode = new byte[msg.Length + 1];
-            Array.Copy(msg, 0, msgWithOpcode, 1, msg.Length);
-            msgWithOpcode[0] = 0x82;
-            bytesSent = sender.Send(msgWithOpcode);
+            byte[] buffer1 = new byte[1000];
+            byte[] buffer2 = new byte[1000];
+            sender.Receive(buffer1);
+            sender.Receive(buffer2);
+            byte[] msg2 = Encoding.ASCII.GetBytes("0.0.0.0~125.32.192.13~124.234.134.54~145.3.21.94~255.255.255.255\n");
+            byte[] msg2WithOpcode = new byte[msg2.Length + 1];
+            Array.Copy(msg2, 0, msg2WithOpcode, 1, msg2.Length);
+            msg2WithOpcode[0] = 0x83;
+            bytesSent = sender.Send(msg2WithOpcode);
+            Console.WriteLine("Sent IP traceroute data");
+
+            Thread.Sleep(100);
+            byte[] msg3 = Encoding.ASCII.GetBytes("`1234567890-=~!@#$%^&*()_+qwertyuiop[]\\QWERTYUIOP{}|asdfghjkl;ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>?\n");
+            byte[] msg3WithOpcode = new byte[msg3.Length + 1];
+            Array.Copy(msg3, 0, msg3WithOpcode, 1, msg3.Length);
+            msg3WithOpcode[0] = 0x82;
+            bytesSent = sender.Send(msg3WithOpcode);
             Console.WriteLine("Sent keylog data");
             sender.Close();
-        }*/
+        }
 
-        static void test3()
+        /*static void test3()
         {
             Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             sender.Connect(remoteEP);
@@ -79,14 +91,14 @@ namespace SimpleTCPClient
             int stolenCode = buffer[0];
             Console.WriteLine("Received stolen code: {0}", stolenCode);
 
-            msg = Encoding.ASCII.GetBytes("125.32.192.13~124.234.134.54~145.3.21.94\n");
+            msg = Encoding.ASCII.GetBytes("0.0.0.0~125.32.192.13~124.234.134.54~145.3.21.94~255.255.255.255\n");
             byte[] msgWithOpcode = new byte[msg.Length + 1];
             Array.Copy(msg, 0, msgWithOpcode, 1, msg.Length);
             msgWithOpcode[0] = 0x83;
             bytesSent = sender.Send(msgWithOpcode);
             Console.WriteLine("Sent IP traceroute data");
             sender.Close();
-        }
+        }*/
 
         /*static void test4()
         {
@@ -129,8 +141,8 @@ namespace SimpleTCPClient
         static void Main(string[] args)
         {
             test1();
-            //test2();
-            test3();
+            test2();
+            //test3();
             //test4();
         }
     }
